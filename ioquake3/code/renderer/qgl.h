d38138805b3c6170149b873797bbcd66d6d1e651
thunderbird2k <thunderbird2k@da33a61e-047f-11df-949c-ffe96a81eb6d>
2010-01-23 14:11:00 +0000

    We don't need egl.h and Android doesn't contain it by default anyway
    git-svn-id: http://kwaak3.googlecode.com/svn/trunk@9 da33a61e-047f-11df-949c-ffe96a81eb6d

--- ioquake3/code/renderer/qgl.h
+++ ioquake3/code/renderer/qgl.h
@@ -47,7 +47,6 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 #elif defined( __linux__ )
 
-#include <GLES/egl.h>
 #include <GLES/gl.h>
 
 #include "../android/android_glimp.h"
